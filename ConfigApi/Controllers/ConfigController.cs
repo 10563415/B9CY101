803648aceb0d76faa98bdd3cfe3e9de853cae04b
samarth <30378006+samarth1989@users.noreply.github.com>
2021-03-25 20:34:19 +0000

    Added Config Api code

---
+++ ConfigApi/Controllers/ConfigController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ConfigApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : Controller
+    {
+
+        private readonly IConfiguration _config;
+
+        public ConfigController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet("GetConfiguration")]
+        public string GetConfiguration()
+        {
+            
+            return "Response from Config API" ;
+
+        }
+    }
+}
