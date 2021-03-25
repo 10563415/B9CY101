88afe062429c4c9da8c0ccd77707a0c7506a70fd
samarth <30378006+samarth1989@users.noreply.github.com>
2021-03-25 20:56:54 +0000

    Added Employee Api

---
+++ EmployeeApi/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EmployeeApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("GetEmployeeDetails")]
+        public object GetEmployeeDetails()
+        {
+            Console.WriteLine("Inside the Employee API");
+            return "Response from Employee API";
+
+
+        }
+    }
+}
