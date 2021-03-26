0ce061f94a3ce4de256c734d743c18f2cc1d2caf
samarth <30378006+samarth1989@users.noreply.github.com>
2021-03-26 03:01:12 +0000

    Corrected structure as per docker conventions

--- EmployeeApi/Startup.cs
+++ EmployeeApi/Startup.cs
@@ -30,7 +30,7 @@ namespace EmployeeApi
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmployeeAPi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmployeeApi", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace EmployeeApi
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeAPi v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeApi v1"));
             }
 
             app.UseHttpsRedirection();
